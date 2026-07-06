516ae285031778751584f03738fa654a10abbe4d
Sadasiba Nayak <nayaksadasiba02@gmail.com>
2026-07-06 16:21:08 +0530

    structure of project

---
+++ src/main/java/com/cyber_employee_portal/CyberEmployeePortalApplication.java
@@ -0,0 +1,13 @@
+package com.cyber_employee_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberEmployeePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberEmployeePortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyber_employee_portal/CyberEmployeePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyber_employee_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberEmployeePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
