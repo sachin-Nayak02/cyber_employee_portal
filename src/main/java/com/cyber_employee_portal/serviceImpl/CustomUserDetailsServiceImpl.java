170d4f55d7a8ce0c3b0f18e1461bee72e386f08b
sachin-Nayak02 <nayaksadasiba02@gmail.com>
2026-07-08 17:28:32 +0530

    login part is done

---
+++ src/main/java/com/cyber_employee_portal/serviceImpl/CustomUserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.cyber_employee_portal.serviceImpl;
+
+import com.cyber_employee_portal.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsServiceImpl implements UserDetailsService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return employeeRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("No employee found with email: " + email));
+    }
+}
